166ba3eff78c03f0b352fa16d6243120615bc0f9
Daniel Cho <danielcho91@gmail.com>
2020-08-29 19:39:33 -0400

    move private info into dev.js

--- index.js
+++ index.js
@@ -1,22 +1,39 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const config = require('./config/keys');
+
+const { User } = require('./models/user');
 
 mongoose
-  .connect(
-    'mongodb+srv://dan:12345@cluster0.rys5k.mongodb.net/<dbname>?retryWrites=true&w=majority',
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    }
-  )
+  .connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(() => {
     console.log('DB connected');
   })
   .catch((err) => console.log(err));
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
 app.get('/', (req, res) => {
-  res.send('hello world');
+  res.json({ 'hello~~': 'hiasdsadas' });
+});
+
+app.post('/api/users/register', (req, res) => {
+  const user = new User(req.body);
+  user.save((err, userData) => {
+    if (err) {
+      return res.json({ success: false, err });
+    }
+    res.status(200).json({ success: true, userData: userData });
+  });
 });
 
 app.listen(5000);
